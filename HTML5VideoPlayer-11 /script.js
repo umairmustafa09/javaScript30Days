f4a3f8a7c45061fe138b653a5bc763bcf5b0561a
Umair Mustafa <umairhassaaan@gmail.com>
2020-03-04 19:19:51 +0500

    const player = document.querySelector('.player');

---
+++ HTML5VideoPlayer-11 /script.js
@@ -0,0 +1,25 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const ranges = player.querySelectorAll('.sliders');
+
+function handlerClick() {
+	const method = this.paused ? 'play' : 'pause';
+	video[method]();
+}
+
+function handlerChange(e) {
+	if (this.name === 'currentTime') this.max = video.duration;
+	const percent = parseFloat(this.value);
+	video[this.name] = percent;
+}
+
+function moveProgressBar() {
+	const progressBar = document.querySelector('.progressBar');
+	progressBar.max = video.duration;
+	progressBar.value = video.currentTime;
+}
+
+video.addEventListener('click', handlerClick);
+video.addEventListener('click', handlerClick);
+video.addEventListener('timeupdate', moveProgressBar);
+ranges.forEach((range) => range.addEventListener('change', handlerChange));
